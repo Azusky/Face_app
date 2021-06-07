f1067c8a12d5dfa5c548afc20616b801d35350d8
Azusky <68742696+Azusky@users.noreply.github.com>
2021-06-08 00:01:06 +0300

    Update detecter.py

--- scraping/checkDublicatImg/detecter.py
+++ scraping/checkDublicatImg/detecter.py
@@ -1,35 +1,16 @@
 import os
-from PIL import Image, ImageStat
-
-image_folder = 'images'
-image_files = [_ for _ in os.listdir(image_folder) if _.endswith('jpeg')]
-
-# print(image_files)
-
-# dublicate_files = []
-
-# for file_org in image_files:
-#     if not file_org in dublicate_files:
-#         image_org = Image.open(os.path.join(image_folder, file_org))
-#         pix_meanl = ImageStat.Stat(image_org).mean
-
-#         for file_check in image_files:
-#             if file_check != file_org:
-#                 image_check = Image.open(os.path.join(image_folder, file_check))
-#                 pix_mean2  = ImageStat.Stat(image_check).mean
-
-#                 if pix_meanl == pix_mean2:
-
-#                     dublicate_files.append(file_org)
-#                     dublicate_files.append(file_check)
-
-
-print(image_files)
-
 from skimage.metrics import structural_similarity as ssim
 
 import numpy as np
 import cv2
+
+image_folder = 'images'
+image_files = [_ for _ in os.listdir(image_folder) if _.endswith('jpeg')]
+
+
+print(image_files)
+
+
 
 
 def mse(imageA, imageB):
